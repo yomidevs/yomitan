8292be0243c3478845a851aadcbf476660d86d7f
Alex Yatskov <alex@foosoft.net>
2016-04-24 19:09:11 -0700

    Adding template auto build script

---
+++ build_tmpl_auto.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DIRECTORY_TO_OBSERVE="util/tmpl"
+BUILD_SCRIPT="build_tmpl.sh"
+
+function block_for_change {
+    inotifywait -e modify,move,create,delete $DIRECTORY_TO_OBSERVE
+}
+
+function build {
+    bash $BUILD_SCRIPT
+}
+
+build
+while block_for_change; do
+  build
+done
