8f8d8d3f513517fdca941bc3fb0bc88722229db5
Alex Yatskov <alex@foosoft.net>
2016-08-13 11:56:56 -0700

    Delete old script

--- build_dict.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-KANJIDIC=common/data/kanjidic
-EDICT=common/data/edict
-ENAMDICT=common/data/enamdict
-DICT_DIR=ext/bg/data
-
-common/compile.py --kanjidic $KANJIDIC --edict $EDICT --enamdict $ENAMDICT $DICT_DIR
