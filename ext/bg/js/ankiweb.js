6c940656ca5ec5a004d402ffd2fcd9a148bd71b4
Alex Yatskov <alex@foosoft.net>
2016-12-29 11:10:12 -0800

    GET unless POSTing data

--- ext/bg/js/ankiweb.js
+++ ext/bg/js/ankiweb.js
@@ -155,9 +155,14 @@ class AnkiWeb {
             const xhr = new XMLHttpRequest();
             xhr.addEventListener('error', () => reject('failed to execute network request'));
             xhr.addEventListener('load', () => resolve(xhr.responseText));
-            xhr.open('POST', url);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.send(dataEnc);
+            if (dataEnc) {
+                xhr.open('POST', url);
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                xhr.send(dataEnc);
+            } else {
+                xhr.open('GET', url);
+                xhr.send();
+            }
         });
     }
 }
