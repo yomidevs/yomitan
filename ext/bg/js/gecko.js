dfe97a2b775bea4e68bb43a6d02e7a624b9269f7
Alex Yatskov <alex@foosoft.net>
2017-02-26 12:14:22 -0800

    fixup of gecko polyfill

--- ext/bg/js/gecko.js
+++ ext/bg/js/gecko.js
@@ -1,16 +1,35 @@
-//
-// Gecko does not currently support chrome.storage.sync, use storage.local instead
-// https://bugzilla.mozilla.org/show_bug.cgi?id=1220494
+/*
+ * Copyright (c) 2017 Alex Yatskov <alex@foosoft.net>
+ * Author: Alex Yatskov <alex@foosoft.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 if (!chrome.storage.sync) {
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1220494
     chrome.storage.sync = chrome.storage.local;
 }
 
-// Gecko does not currently support chrome.runtime.onInstalled, just ignore calls to it
-// (https://bugzilla.mozilla.org/show_bug.cgi?id=1252871)
 if (!chrome.runtime.onInstalled) {
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1252871
     chrome.runtime.onInstalled = {
-        'addListener' : function(){},
-        'hasListener' : function(){},
-        'removeListener' : function(){}
+        'addListener': () => {},
+        'hasListener': () => {},
+        'removeListener': () => {}
     };
 }
--- ext/fg/js/gecko.js
+++ ext/fg/js/gecko.js
@@ -1,10 +1,33 @@
-if (!document.caretRangeFromPoint){
-    document.caretRangeFromPoint = function polyfillcaretRangeFromPoint(x,y){
-        let range = document.createRange();
-        let position = document.caretPositionFromPoint(x,y);
-        if (!position) {
+/*
+ * Copyright (c) 2017 Alex Yatskov <alex@foosoft.net>
+ * Author: Alex Yatskov <alex@foosoft.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+if (!document.caretRangeFromPoint) {
+    document.caretRangeFromPoint = (x, y) => {
+        const position = document.caretPositionFromPoint(x,y);
+        if (position === null) {
             return null;
         }
+
+        const range = document.createRange();
         range.setStart(position.offsetNode, position.offset);
         range.setEnd(position.offsetNode, position.offset);
         return range;
