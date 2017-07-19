26e1cc517f6100fe665d8d066729a11fde2cdc26
Alex Yatskov <alex@foosoft.net>
2017-07-18 23:07:46 -0700

    refactor

---
+++ ext/bg/js/dictionary.js
@@ -0,0 +1,233 @@
+/*
+ * Copyright (C) 2016  Alex Yatskov <alex@foosoft.net>
+ * Author: Alex Yatskov <alex@foosoft.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+function dictEnabledSet(options) {
+    const dictionaries = {};
+    for (const title in options.dictionaries) {
+        const dictionary = options.dictionaries[title];
+        if (dictionary.enabled) {
+            dictionaries[title] = dictionary;
+        }
+    }
+
+    return dictionaries;
+}
+
+function dictConfigured(options) {
+    for (const title in options.dictionaries) {
+        if (options.dictionaries[title].enabled) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function dictRowsSort(rows, options) {
+    return rows.sort((ra, rb) => {
+        const pa = (options.dictionaries[ra.title] || {}).priority || 0;
+        const pb = (options.dictionaries[rb.title] || {}).priority || 0;
+        if (pa > pb) {
+            return -1;
+        } else if (pa < pb) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+}
+
+function dictTermsSort(definitions, dictionaries=null) {
+    return definitions.sort((v1, v2) => {
+        const sl1 = v1.source.length;
+        const sl2 = v2.source.length;
+        if (sl1 > sl2) {
+            return -1;
+        } else if (sl1 < sl2) {
+            return 1;
+        }
+
+        if (dictionaries !== null) {
+            const p1 = (dictionaries[v1.dictionary] || {}).priority || 0;
+            const p2 = (dictionaries[v2.dictionary] || {}).priority || 0;
+            if (p1 > p2) {
+                return -1;
+            } else if (p1 < p2) {
+                return 1;
+            }
+        }
+
+        const s1 = v1.score;
+        const s2 = v2.score;
+        if (s1 > s2) {
+            return -1;
+        } else if (s1 < s2) {
+            return 1;
+        }
+
+        const rl1 = v1.reasons.length;
+        const rl2 = v2.reasons.length;
+        if (rl1 < rl2) {
+            return -1;
+        } else if (rl1 > rl2) {
+            return 1;
+        }
+
+        return v2.expression.localeCompare(v1.expression);
+    });
+}
+
+function dictTermsUndupe(definitions) {
+    const definitionGroups = {};
+    for (const definition of definitions) {
+        const definitionExisting = definitionGroups[definition.id];
+        if (!definitionGroups.hasOwnProperty(definition.id) || definition.expression.length > definitionExisting.expression.length) {
+            definitionGroups[definition.id] = definition;
+        }
+    }
+
+    const definitionsUnique = [];
+    for (const key in definitionGroups) {
+        definitionsUnique.push(definitionGroups[key]);
+    }
+
+    return definitionsUnique;
+}
+
+function dictTermsGroup(definitions, dictionaries) {
+    const groups = {};
+    for (const definition of definitions) {
+        const key = [definition.source, definition.expression].concat(definition.reasons);
+        if (definition.reading) {
+            key.push(definition.reading);
+        }
+
+        const group = groups[key];
+        if (group) {
+            group.push(definition);
+        } else {
+            groups[key] = [definition];
+        }
+    }
+
+    const results = [];
+    for (const key in groups) {
+        const groupDefs = groups[key];
+        const firstDef = groupDefs[0];
+        dictTermsSort(groupDefs, dictionaries);
+        results.push({
+            definitions: groupDefs,
+            expression: firstDef.expression,
+            reading: firstDef.reading,
+            reasons: firstDef.reasons,
+            score: groupDefs.reduce((p, v) => v.score > p ? v.score : p, Number.MIN_SAFE_INTEGER),
+            source: firstDef.source
+        });
+    }
+
+    return dictTermsSort(results);
+}
+
+function dictTagBuildSource(name) {
+    return dictTagSanitize({name, category: 'dictionary', order: 100});
+}
+
+function dictTagBuild(name, meta) {
+    const tag = {name};
+    const symbol = name.split(':')[0];
+    for (const prop in meta[symbol] || {}) {
+        tag[prop] = meta[symbol][prop];
+    }
+
+    return dictTagSanitize(tag);
+}
+
+function dictTagSanitize(tag) {
+    tag.name = tag.name || 'untitled';
+    tag.category = tag.category || 'default';
+    tag.notes = tag.notes || '';
+    tag.order = tag.order || 0;
+    return tag;
+}
+
+function dictTagsSort(tags) {
+    return tags.sort((v1, v2) => {
+        const order1 = v1.order;
+        const order2 = v2.order;
+        if (order1 < order2) {
+            return -1;
+        } else if (order1 > order2) {
+            return 1;
+        }
+
+        const name1 = v1.name;
+        const name2 = v2.name;
+        if (name1 < name2) {
+            return -1;
+        } else if (name1 > name2) {
+            return 1;
+        }
+
+        return 0;
+    });
+}
+
+function dictFieldSplit(field) {
+    return field.length === 0 ? [] : field.split(' ');
+}
+
+function dictFieldFormat(field, definition, mode, options) {
+    const markers = [
+        'audio',
+        'character',
+        'cloze-body',
+        'cloze-prefix',
+        'cloze-suffix',
+        'dictionary',
+        'expression',
+        'furigana',
+        'glossary',
+        'glossary-brief',
+        'kunyomi',
+        'onyomi',
+        'reading',
+        'sentence',
+        'tags',
+        'url'
+    ];
+
+    for (const marker of markers) {
+        const data = {
+            marker,
+            definition,
+            group: options.general.groupResults,
+            html: options.anki.htmlCards,
+            modeTermKanji: mode === 'term-kanji',
+            modeTermKana: mode === 'term-kana',
+            modeKanji: mode === 'kanji'
+        };
+
+        field = field.replace(
+            `{${marker}}`,
+            Handlebars.templates['fields.html'](data).trim()
+        );
+    }
+
+    return field;
+}
