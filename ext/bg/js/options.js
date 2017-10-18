2cbbcd242e3eeeb4d7a6167d7917d53bd9424551
siikamiika <siikamiika@users.noreply.github.com>
2017-10-18 10:08:34 +0300

    fixups: remove useless check

--- ext/bg/js/options.js
+++ ext/bg/js/options.js
@@ -269,14 +269,11 @@ function optionsVersion(options) {
             }
             options.general.compactTags = false;
             options.general.compactGlossaries = false;
-            if (options.anki.fieldTemplates !== optionsFieldTemplates()) {
-                options.anki.fieldTemplates = '{{#if merge}}\n' +
-                                            optionsFieldTemplates() +
-                                            '\n{{else}}\n' +
-                                            options.anki.fieldTemplates +
-                                            '\n{{/if}}';
-
-            }
+            options.anki.fieldTemplates = '{{#if merge}}\n' +
+                                        optionsFieldTemplates() +
+                                        '\n{{else}}\n' +
+                                        options.anki.fieldTemplates +
+                                        '\n{{/if}}';
         }
     ];
 
