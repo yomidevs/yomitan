609dbf6a819a736818e9caa03893850f14453d84
siikamiika <siikamiika@users.noreply.github.com>
2019-08-15 15:35:23 +0300

    ensure that content is iterable

--- ext/fg/js/source.js
+++ ext/fg/js/source.js
@@ -240,7 +240,7 @@ class TextSourceElement {
 
         let consumed = 0;
         let content = '';
-        for (let currentChar of this.content) {
+        for (let currentChar of this.content || '') {
             if (consumed >= length) {
                 break;
             } else if (!currentChar.match(IGNORE_TEXT_PATTERN)) {
