f6f19dc9deeaf6ae88d36b776d2f1f4d0ef40bd3
siikamiika <siikamiika@users.noreply.github.com>
2019-11-13 00:37:09 +0200

    typo

--- ext/bg/js/options.js
+++ ext/bg/js/options.js
@@ -314,7 +314,7 @@ function profileOptionsCreateDefaults() {
         parsing: {
             enableScanningParser: true,
             enableMecabParser: false,
-            selectedParser = null
+            selectedParser: null
         },
 
         anki: {
