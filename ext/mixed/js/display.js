yomidevs/yomitan

commit 4e7d08ff2c184a361622a6efaf00e21af51428a9

set current entry even when disableScroll is true

ext/mixed/js/display.js
@@ -360,6 +360,8 @@ class Display {
             const {index, scroll, disableScroll} = context;
             if (!disableScroll) {
                 this.entryScrollIntoView(index || 0, scroll);
+            } else {
+                this.entrySetCurrent(index || 0);
             }
 
             if (options.audio.enabled && options.audio.autoPlay) {
@@ -465,7 +467,7 @@ class Display {
         }
     }
 
-    entryScrollIntoView(index, scroll, smooth) {
+    entrySetCurrent(index) {
         index = Math.min(index, this.definitions.length - 1);
         index = Math.max(index, 0);
 
@@ -479,9 +481,16 @@ class Display {
             entry.classList.add('entry-current');
         }
 
-        this.windowScroll.stop();
-        let target;
+        this.index = index;
 
+        return entry;
+    }
+
+    entryScrollIntoView(index, scroll, smooth) {
+        this.windowScroll.stop();
+
+        const entry = this.entrySetCurrent(index);
+        let target;
         if (scroll !== null) {
             target = scroll;
         } else {
@@ -493,8 +502,6 @@ class Display {
         } else {
             this.windowScroll.toY(target);
         }
-
-        this.index = index;
     }
 
     sourceTermView() {

commit 403b86675d288a8082125475f87f9b1740e3480a

fix entry scroll index 0 bug
Introduced in 4e7d08ff2c184a361622a6efaf00e21af51428a9 because the
range-limited index value isn't kept in the same scope.

ext/mixed/js/display.js
@@ -515,7 +515,7 @@ class Display {
         if (scroll !== null) {
             target = scroll;
         } else {
-            target = index === 0 || entry === null ? 0 : Display.getElementTop(entry);
+            target = this.index === 0 || entry === null ? 0 : Display.getElementTop(entry);
         }
 
         if (smooth) {