22c87c5d6e32e7009efb6a9794593a1be695b4ea
Alex Yatskov <alex@foosoft.net>
2020-01-11 18:32:27 -0800

    Update zipping scripts

--- build_zip.sh
+++ build_zip.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
-ZIP=yomichan.zip
-rm -f $ZIP
-7za a $ZIP ./ext/*
+#!/bin/bash
+
+rm yomichan_source.zip
+7za a yomichan_source.zip ./ext ./LICENSE ./README.md ./resources ./tmpl
+
+rm yomichan_extension.zip
+7za a yomichan_extension.zip ./ext/*
