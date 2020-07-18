ffc0b6588e9f95d873fe87db6ba647a27c0a8b3d
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2020-07-18 14:17:30 -0400

    Fix incorrect URL being used (#669)

--- ext/bg/js/backend.js
+++ ext/bg/js/backend.js
@@ -1301,7 +1301,7 @@ class Backend {
             chrome.tabs.sendMessage(tab.id, {action: 'getUrl'}, {frameId: 0}, (response) => {
                 let url = null;
                 try {
-                    url = yomichan.getMessageResponseResult(response);
+                    ({url} = yomichan.getMessageResponseResult(response));
                 } catch (error) {
                     // NOP
                 }
