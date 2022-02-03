89ac85afd03e62818624b507c91569edbec54f3d
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2022-02-02 20:43:10 -0500

    Update copyright date (#2062)
    * Update eslint settings
    
    * Update 2021 files
    
    * Update other files

--- ext/js/pages/action-popup-main.js
+++ ext/js/pages/action-popup-main.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2021  Yomichan Authors
+ * Copyright (C) 2017-2022  Yomichan Authors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
