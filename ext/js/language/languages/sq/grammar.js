6c969409013800cc8ea752f38e0d6b8ad859c0a8
Stefan Vukovic <stefanvukovic44@gmail.com>
2023-10-11 17:13:19 +0200

    expand Albanian grammar

--- ext/js/language/languages/sq/grammar.js
+++ ext/js/language/languages/sq/grammar.js
@@ -23,36 +23,106 @@ window.languages.sq.getDeinflectionReasons = async () => {
     return new Map([
         // Nouns
         ['definite', [
+            // masculine
             suffixInflection('ku', 'k', [], ['noun']),
             suffixInflection('gu', 'g', [], ['noun']),
             suffixInflection('hu', 'h', [], ['noun']),
             suffixInflection('au', 'a', [], ['noun']),
             suffixInflection('iu', 'i', [], ['noun']),
-            suffixInflection('eu', 'e', [], ['noun'])
+            suffixInflection('eu', 'e', [], ['noun']),
+            suffixInflection('i', 'ë', [], ['noun']),
+            suffixInflection('i', '', [], ['noun']),
+            suffixInflection('ri', '', [], ['noun']),
+            suffixInflection('oi', 'ua', [], ['noun']),
+            // feminine
+            suffixInflection('a', 'ë', [], ['noun']),
+            suffixInflection('a', '', [], ['noun']),
+            suffixInflection('ja', 'e', [], ['noun'])
         ]],
         ['plural', [
             suffixInflection('e', '', [], ['noun'])
         ]],
         // Verbs
-        [['present', 'indicative', 'second-person', ' singular'], [
-            suffixInflection('n', 'j', [], ['verb'])
+        [['present', 'indicative', 'second-person', 'singular'], [
+            suffixInflection('on', 'oj', [], ['verb']),
+            suffixInflection('hesh', 'hem', [], ['verb'])
         ]],
         [['present', 'indicative', 'third-person', 'singular'], [
-            suffixInflection('n', 'j', [], ['verb'])
+            suffixInflection('on', 'oj', [], ['verb']),
+            suffixInflection('het', 'hem', [], ['verb'])
         ]],
         [['present', 'indicative', 'first-person', 'plural'], [
-            suffixInflection('jmë', 'j', [], ['verb']),
+            suffixInflection('ojmë', 'oj', [], ['verb']),
             suffixInflection('im', '', [], ['verb']),
-            suffixInflection('më', '', [], ['verb'])
+            suffixInflection('më', '', [], ['verb']),
+            suffixInflection('hemi', 'hem', [], ['verb'])
         ]],
         [['present', 'indicative', 'second-person', 'plural'], [
-            suffixInflection('ni', 'j', [], ['verb']),
-            suffixInflection('ni', '', [], ['verb'])
+            suffixInflection('oni', 'oj', [], ['verb']),
+            suffixInflection('ni', '', [], ['verb']),
+            suffixInflection('heni', 'hem', [], ['verb'])
         ]],
         [['present', 'indicative', 'third-person', 'plural'], [
-            suffixInflection('jnë', 'j', [], ['verb']),
+            suffixInflection('ojnë', 'oj', [], ['verb']),
             suffixInflection('in', '', [], ['verb']),
-            suffixInflection('në', '', [], ['verb'])
+            suffixInflection('në', '', [], ['verb']),
+            suffixInflection('hen', 'hem', [], ['verb'])
+        ]],
+        [['aorist', 'first-person', 'singular', 'indicative'], [
+            suffixInflection('ova', 'oj', [], ['verb'])
+        ]],
+        [['aorist', 'second-person', 'singular', 'indicative'], [
+            suffixInflection('ove', 'oj', [], ['verb'])
+        ]],
+        [['aorist', 'third-person', 'singular', 'indicative'], [
+            suffixInflection('oi', 'oj', [], ['verb'])
+        ]],
+        [['aorist', 'first-person', 'plural', 'indicative'], [
+            suffixInflection('uam', 'oj', [], ['verb'])
+        ]],
+        [['aorist', 'second-person', 'plural', 'indicative'], [
+            suffixInflection('uat', 'oj', [], ['verb'])
+        ]],
+        [['aorist', 'third-person', 'plural', 'indicative'], [
+            suffixInflection('uan', 'oj', [], ['verb'])
+        ]],
+        [['imperfect', 'first-person', 'singular', 'indicative'], [
+            suffixInflection('oja', 'oj', [], ['verb']),
+            suffixInflection('hesha', 'hem', [], ['verb'])
+        ]],
+        [['imperfect', 'second-person', 'singular', 'indicative'], [
+            suffixInflection('oje', 'oj', [], ['verb']),
+            suffixInflection('heshe', 'hem', [], ['verb'])
+        ]],
+        [['imperfect', 'third-person', 'singular', 'indicative'], [
+            suffixInflection('onte', 'oj', [], ['verb']),
+            suffixInflection('hej', 'hem', [], ['verb'])
+        ]],
+        [['imperfect', 'first-person', 'plural', 'indicative'], [
+            suffixInflection('onim', 'oj', [], ['verb']),
+            suffixInflection('heshim', 'hem', [], ['verb'])
+        ]],
+        [['imperfect', 'second-person', 'plural', 'indicative'], [
+            suffixInflection('onit', 'oj', [], ['verb']),
+            suffixInflection('heshit', 'hem', [], ['verb'])
+        ]],
+        [['imperfect', 'third-person', 'plural', 'indicative'], [
+            suffixInflection('onin', 'oj', [], ['verb']),
+            suffixInflection('heshin', 'hem', [], ['verb'])
+        ]],
+        [['imperative', 'second-person', 'singular', 'present'], [
+            suffixInflection('o', 'oj', [], ['verb'])
+        ]],
+        [['imperative', 'second-person', 'plural', 'present'], [
+            suffixInflection('oni', 'oj', [], ['verb'])
+        ]],
+        ['participle', [
+            suffixInflection('uar', 'oj', [], ['verb']),
+            suffixInflection('ur', '', [], ['verb'])
+        ]],
+        ['mediopassive', [
+            suffixInflection('hem', 'h', ['verb'], ['verb']),
+            suffixInflection('hem', 'j', ['verb'], ['verb'])
         ]]
     ]);
 };
