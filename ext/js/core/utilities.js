46b782b175ef4bc83bb851a954ea6219341c1fc1
Stefan Vukovic <stefanvukovic44@gmail.com>
2024-02-03 21:19:16 +0100

    remove extra line

--- ext/js/core/utilities.js
+++ ext/js/core/utilities.js
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-
 /**
  * Checks whether a given value is a non-array object.
  * @param {unknown} value The value to check.
