9c61d6a1b5526207fc5238f90240d9726e79f594
Cashew <tungbinem@gmail.com>
2024-02-07 07:52:22 +0700

    move destructuring after suffixHeuristic check

--- ext/js/language/language-transformer.js
+++ ext/js/language/language-transformer.js
@@ -137,9 +137,10 @@ export class LanguageTransformer {
         const results = [this._createTransformedText(sourceText, 0, [])];
         for (let i = 0; i < results.length; ++i) {
             const {text, conditions, trace} = results[i];
-            for (const {name, rules, suffixHeuristic} of this._transforms) {
-                if (!suffixHeuristic.test(text)) { continue; }
+            for (const transform of this._transforms) {
+                if (!transform.suffixHeuristic.test(text)) { continue; }
 
+                const {name, rules} = transform;
                 for (let j = 0, jj = rules.length; j < jj; ++j) {
                     const rule = rules[j];
                     if (!LanguageTransformer.conditionsMatch(conditions, rule.conditionsIn)) { continue; }
