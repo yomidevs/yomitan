ba31be64550725076f1d2e77f2c31b0218535a7d
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2024-02-10 15:19:55 -0500

    Actually save and commit changes

--- dev/lib/z-worker.js
+++ dev/lib/z-worker.js
@@ -14,4 +14,5 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 import '../../node_modules/@zip.js/zip.js/lib/z-worker.js';
