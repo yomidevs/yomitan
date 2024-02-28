92af6e2b8cd4295c196226f5f9a4af50b96c6984
Cashew <tungbinem@gmail.com>
2024-02-28 17:50:27 +0900

    fix duplicated title

--- ext/js/background/backend.js
+++ ext/js/background/backend.js
@@ -1556,7 +1556,7 @@ export class Backend {
         return (
             isObject(chrome.action) &&
             typeof chrome.action.getTitle === 'function' ?
-                new Promise((resolve) => { chrome.action.getTitle({}, resolve); }) :
+                new Promise((resolve) => { chrome.action.getTitle({}, (title) => { resolve(title.split(' - ')[0]) }); }) :
                 Promise.resolve('')
         );
     }
