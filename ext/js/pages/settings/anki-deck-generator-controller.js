5d92b1ded5e0a297d0fbe6821500f7112f71b872
kuuuube <hexagonisalie@gmail.com>
2024-05-09 19:50:32 -0400

    Remove unneeded Yomichan mention

--- ext/js/pages/settings/anki-deck-generator-controller.js
+++ ext/js/pages/settings/anki-deck-generator-controller.js
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2023-2024  Yomitan Authors
- * Copyright (C) 2019-2022  Yomichan Authors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
