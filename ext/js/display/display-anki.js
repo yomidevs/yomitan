2bcb0bccc251722b2eff8a0d07e02bca052fc2e1
Kuuuube <61125188+Kuuuube@users.noreply.github.com>
2024-05-10 23:27:57 -0400

    Prevent updating dict entry details for anki when anki is disabled (#909)
    * Prevent updating dict entry details for anki when anki is disabled
    
    * Move check to _updateDictionaryEntryDetails
    
    * Fix if

--- ext/js/display/display-anki.js
+++ ext/js/display/display-anki.js
@@ -351,6 +351,7 @@ export class DisplayAnki {
 
     /** */
     async _updateDictionaryEntryDetails() {
+        if (!this._display.getOptions()?.anki.enable) { return; }
         const {dictionaryEntries} = this._display;
         /** @type {?import('core').TokenObject} */
         const token = {};
