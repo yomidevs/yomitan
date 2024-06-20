8f33cb76cbe7c86ad6ee3cd6700c40f82dc7230d
Kuuuube <61125188+Kuuuube@users.noreply.github.com>
2024-06-20 13:10:07 -0400

    Fix scrollbars on info, legal, and issues; add theming to legal and issues (#1096)
    * Add theming support to generic-page-main.js
    
    * Mark scrollbars on info, issues, and legal
    
    * Remove quick-start-guide-main.js in favor of generic-page-main.js
    
    * Default body to hidden on issues and legal

--- ext/js/pages/generic-page-main.js
+++ ext/js/pages/generic-page-main.js
@@ -16,11 +16,28 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import {ThemeController} from '../app/theme-controller.js';
+import {Application} from '../application.js';
 import {DocumentFocusController} from '../dom/document-focus-controller.js';
 import {ExtensionContentController} from './common/extension-content-controller.js';
+import {SettingsController} from './settings/settings-controller.js';
 
-/** Entry point. */
-function main() {
+await Application.main(true, async (application) => {
+    const settingsController = new SettingsController(application);
+    await settingsController.prepare();
+    /** @type {ThemeController} */
+    const themeController = new ThemeController(document.documentElement);
+    themeController.prepare();
+    const optionsFull = await application.api.optionsGetFull();
+    const {profiles, profileCurrent} = optionsFull;
+    const primaryProfile = (profileCurrent >= 0 && profileCurrent < profiles.length) ? profiles[profileCurrent] : null;
+    if (primaryProfile !== null) {
+        themeController.theme = primaryProfile.options.general.popupTheme;
+        themeController.siteOverride = true;
+        themeController.updateTheme();
+    }
+    document.body.hidden = false;
+
     const documentFocusController = new DocumentFocusController();
     documentFocusController.prepare();
 
@@ -28,6 +45,4 @@ function main() {
     extensionContentController.prepare();
 
     document.documentElement.dataset.loaded = 'true';
-}
-
-main();
+});
