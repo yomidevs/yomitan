79688e1dbf06a177606572539cf58ed45cabe283
Khai Truong <khai.truong@codelink.io>
2024-10-07 00:07:08 +0700

    add term tags to {{tags}}

--- ext/js/templates/anki-template-renderer.js
+++ ext/js/templates/anki-template-renderer.js
@@ -285,7 +285,11 @@ export class AnkiTemplateRenderer {
      */
     _mergeTags(args) {
         const [object, isGroupMode, isMergeMode] = /** @type {[object: import('anki-templates').TermDictionaryEntry, isGroupMode: boolean, isMergeMode: boolean]} */ (args);
+        /** @type {import('anki-templates').Tag[][]} */
         const tagSources = [];
+        if (Array.isArray(object.termTags)) {
+            tagSources.push(object.termTags);
+        }
         if (isGroupMode || isMergeMode) {
             const {definitions} = object;
             if (Array.isArray(definitions)) {
@@ -294,7 +298,9 @@ export class AnkiTemplateRenderer {
                 }
             }
         } else {
-            tagSources.push(object.definitionTags);
+            if (Array.isArray(object.definitionTags)) {
+                tagSources.push(object.definitionTags);
+            }
         }
 
         const tags = new Set();
