0b5593ff98ddb13a0a4a09104181a236af2d586a
MarvNC <rotrobmin@gmail.com>
2024-10-28 23:18:32 -0700

    Use Playwright Report Summary Action

--- playwright.config.js
+++ playwright.config.js
@@ -47,7 +47,10 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : void 0,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: [
+        ['html'],
+        ['json', {outputFile: 'results.json'}],
+    ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
