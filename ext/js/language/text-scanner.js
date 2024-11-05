3ad619139ebeeb7aa697653ceb44a9d9ce8009d5
Kuuuube <61125188+Kuuuube@users.noreply.github.com>
2024-11-05 14:20:26 -0500

    Force scan resolution "character" for ja, zh, yue, and ko (#1542)
    * Force scan resolution "character" for ja, zh, yue, ko
    
    * Split out visibility modifiers
    
    * Hide scan resolution setting for jp, zh, yue, ko
    
    * Remove unused visibility modifiers css from search-settings
    
    ---------
    
    Signed-off-by: Kuuuube <61125188+Kuuuube@users.noreply.github.com>

--- ext/js/language/text-scanner.js
+++ ext/js/language/text-scanner.js
@@ -24,6 +24,8 @@ import {clone} from '../core/utilities.js';
 import {anyNodeMatchesSelector, everyNodeMatchesSelector, getActiveModifiers, getActiveModifiersAndButtons, isPointInSelection} from '../dom/document-util.js';
 import {TextSourceElement} from '../dom/text-source-element.js';
 
+const SCAN_RESOLUTION_EXCLUDED_LANGUAGES = new Set(['ja', 'zh', 'yue', 'ko']);
+
 /**
  * @augments EventDispatcher<import('text-scanner').Events>
  */
@@ -487,7 +489,8 @@ export class TextScanner extends EventDispatcher {
                 null
             );
 
-            if (this._scanResolution === 'word' && !disallowExpandStartOffset) {
+            if (this._scanResolution === 'word' && (!disallowExpandStartOffset ||
+            (this._language === null || !SCAN_RESOLUTION_EXCLUDED_LANGUAGES.has(this._language)))) {
                 // Move the start offset to the beginning of the word
                 textSource.setStartOffset(this._scanLength, this._layoutAwareScan, true);
             }
