b6ce90c2cdaa57dd02fcbc330362c00f16162715
kuuuube <hexagonisalie@gmail.com>
2024-11-22 19:02:07 -0500

    Fix flipped OR/AND

--- ext/js/language/text-scanner.js
+++ ext/js/language/text-scanner.js
@@ -489,7 +489,7 @@ export class TextScanner extends EventDispatcher {
                 null
             );
 
-            if (this._scanResolution === 'word' && (!disallowExpandStartOffset ||
+            if (this._scanResolution === 'word' && (!disallowExpandStartOffset &&
             (this._language === null || !SCAN_RESOLUTION_EXCLUDED_LANGUAGES.has(this._language)))) {
                 // Move the start offset to the beginning of the word
                 textSource.setStartOffset(this._scanLength, this._layoutAwareScan, true);
