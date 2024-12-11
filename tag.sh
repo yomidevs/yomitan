353797b9f8911760d393cd3eaccb15ffca52773f
Darius Jahandarie <djahandarie@gmail.com>
2024-12-11 10:05:55 +0900

    tag.sh: only fetch origin remote, auto-increment last digit if there is an existing tag

--- tag.sh
+++ tag.sh
@@ -16,21 +16,29 @@ fi
 echo -e "${YELLOW}Checking if branch is up to date...${NC}"
 
 changed=0
-git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+git remote update origin && git status -uno | grep -q 'Your branch is behind' && changed=1
 if [ $changed = 1 ]; then
 	echo -e "${RED}Please git pull before tagging.${NC}"
 	exit 1
 fi
 
-# get the current date in the format YY.MM.DD
-DATE=$(date +%y.%-m.%-d)
-
-# create plain tag with .0 at end
-TAG=$DATE.0
-
 # Ask user to confirm the commit and the tag name
 echo -e "${YELLOW}Current HEAD of master branch:${NC}"
 git log -1 --decorate
+echo
+
+# get the current date in the format YY.MM.DD
+DATE=$(date +%y.%-m.%-d)
+
+# Check if the tag already exists and increment if necessary
+COUNTER=0
+TAG=$DATE.$COUNTER
+while git rev-parse "$TAG" >/dev/null 2>&1; do
+	# Increment the counter and recreate TAG with DATE
+	echo -e "${YELLOW}Tag $TAG already exists, incrementing.${NC}"
+	COUNTER=$((COUNTER + 1))
+	TAG="$DATE.$COUNTER"
+done
 echo
 echo -e -n "${YELLOW}Tagging current HEAD of master with tag ${TAG}. Are you sure? (y/n): ${NC}"
 read -p "" -n 1 -r
