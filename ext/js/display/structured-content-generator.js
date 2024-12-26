24ae7d0f3d7e5be7712598f8ef0ea9bbb92479c8
kuuuube <hexagonisalie@gmail.com>
2024-12-26 15:34:37 -0500

    Fix background displaying where it shouldnt

--- ext/js/display/structured-content-generator.js
+++ ext/js/display/structured-content-generator.js
@@ -104,10 +104,6 @@ export class StructuredContentGenerator {
         const overlay = this._createElement('span', 'gloss-image-container-overlay');
         imageContainer.appendChild(overlay);
 
-        const linkText = this._createElement('span', 'gloss-image-link-text');
-        linkText.textContent = 'Image';
-        node.appendChild(linkText);
-
         node.dataset.path = path;
         node.dataset.dictionary = dictionary;
         node.dataset.imageLoadState = 'not-loaded';
