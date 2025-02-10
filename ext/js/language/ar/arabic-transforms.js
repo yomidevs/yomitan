a174981305ac8060f7d9a2ffaf402256404155c5
JimBim <jimb1m@proton.me>
2025-02-10 23:50:14 +0000

    Add transforms for Arabic imperfect tense

--- ext/js/language/ar/arabic-transforms.js
+++ ext/js/language/ar/arabic-transforms.js
@@ -17,18 +17,36 @@
 
 import {prefixInflection, suffixInflection} from '../language-transforms.js';
 
+const arabicLetters = '[\u0620-\u065F\u066E-\u06D3\u06D5\u06EE\u06EF\u06FA-\u06FC\u06FF]';
 const directObjectPronouns1st = ['ني', 'نا'];
 const directObjectPronouns2nd = ['ك', 'كما', 'كم', 'كن'];
 const directObjectPronouns3rd = ['ه', 'ها', 'هما', 'هم', 'هن'];
 const directObjectPronouns = [...directObjectPronouns1st, ...directObjectPronouns2nd, ...directObjectPronouns3rd];
+
 /**
- * @template {string} TCondition
+ * @param {string} prefix
+ * @param {boolean} includeLiPrefix
+ * @returns {string[]}
+ */
+function getImperfectPrefixes(prefix, includeLiPrefix = true) {
+    return [
+        `${prefix}`,
+        `و${prefix}`,
+        `ف${prefix}`,
+        `س${prefix}`,
+        `وس${prefix}`,
+        `فس${prefix}`,
+        ...(includeLiPrefix ? [`ل${prefix}`, `ول${prefix}`, `فل${prefix}`] : []),
+    ];
+}
+
+/**
  * @param {string} inflectedPrefix
  * @param {string} deinflectedPrefix
  * @param {string} initialStemSegment
- * @param {TCondition[]} conditionsIn
- * @param {TCondition[]} conditionsOut
- * @returns {import('language-transformer').Rule<TCondition>}
+ * @param {Condition[]} conditionsIn
+ * @param {Condition[]} conditionsOut
+ * @returns {import('language-transformer').Rule<Condition>}
  */
 function conditionalPrefixInflection(inflectedPrefix, deinflectedPrefix, initialStemSegment, conditionsIn, conditionsOut) {
     const prefixRegExp = new RegExp('^' + inflectedPrefix + initialStemSegment);
@@ -42,13 +60,12 @@ function conditionalPrefixInflection(inflectedPrefix, deinflectedPrefix, initial
 }
 
 /**
- * @template {string} TCondition
  * @param {string} inflectedSuffix
  * @param {string} deinflectedSuffix
  * @param {string} finalStemSegment
- * @param {TCondition[]} conditionsIn
- * @param {TCondition[]} conditionsOut
- * @returns {import('language-transformer').SuffixRule<TCondition>}
+ * @param {Condition[]} conditionsIn
+ * @param {Condition[]} conditionsOut
+ * @returns {import('language-transformer').SuffixRule<Condition>}
  */
 function conditionalSuffixInflection(inflectedSuffix, deinflectedSuffix, finalStemSegment, conditionsIn, conditionsOut) {
     const suffixRegExp = new RegExp(finalStemSegment + inflectedSuffix + '$');
@@ -60,6 +77,144 @@ function conditionalSuffixInflection(inflectedSuffix, deinflectedSuffix, finalSt
         conditionsIn,
         conditionsOut,
     };
+}
+
+/**
+ * @param {string} inflectedPrefix
+ * @param {string} deinflectedPrefix
+ * @param {string} inflectedSuffix
+ * @param {string} deinflectedSuffix
+ * @param {Condition[]} conditionsIn
+ * @param {Condition[]} conditionsOut
+ * @param {object} [options={}]
+ * @param {string} [options.initialStemSegment = '']
+ * @param {string} [options.finalStemSegment = '']
+ * @returns {import('language-transformer').Rule<Condition>}
+ */
+function sandwichInflection(
+    inflectedPrefix,
+    deinflectedPrefix,
+    inflectedSuffix,
+    deinflectedSuffix,
+    conditionsIn,
+    conditionsOut,
+    {initialStemSegment = '', finalStemSegment = ''} = {},
+) {
+    if (!inflectedSuffix && !deinflectedSuffix) {
+        return conditionalPrefixInflection(
+            inflectedPrefix,
+            deinflectedPrefix,
+            initialStemSegment,
+            conditionsIn,
+            conditionsOut,
+        );
+    }
+    if (!inflectedPrefix && !deinflectedPrefix) {
+        return conditionalSuffixInflection(
+            inflectedSuffix,
+            deinflectedSuffix,
+            finalStemSegment,
+            conditionsIn,
+            conditionsOut,
+        );
+    }
+
+    const regex = new RegExp(
+        `^${inflectedPrefix}${initialStemSegment}${arabicLetters}+${finalStemSegment}${inflectedSuffix}$`,
+    );
+    return {
+        type: 'other',
+        isInflected: regex,
+        deinflect: (text) => deinflectedPrefix + text.slice(inflectedPrefix.length, -inflectedSuffix.length) + deinflectedSuffix,
+        conditionsIn,
+        conditionsOut,
+    };
+}
+
+/**
+ * @param {string} inflectedPrefix
+ * @param {string} deinflectedPrefix
+ * @param {string} inflectedSuffix
+ * @param {string} deinflectedSuffix
+ * @param {object} [options={}]
+ * @param {string} [options.attachedSuffix = inflectedSuffix]
+ * @param {boolean} [options.attachesTo1st = true]
+ * @param {boolean} [options.attachesTo2nd = true]
+ * @param {boolean} [options.includeLiPrefix = true]
+ * @param {string} [options.initialStemSegment = '']
+ * @param {string} [options.finalStemSegment = '']
+ * @returns {import('language-transformer').Rule<Condition>[]}
+ */
+function getImperfectRules(
+    inflectedPrefix,
+    deinflectedPrefix,
+    inflectedSuffix,
+    deinflectedSuffix,
+    {
+        attachedSuffix = inflectedSuffix,
+        attachesTo1st = true,
+        attachesTo2nd = true,
+        includeLiPrefix = true,
+        initialStemSegment = '',
+        finalStemSegment = '',
+    } = {},
+) {
+    const stemSegments = {initialStemSegment, finalStemSegment};
+    const rules = getImperfectPrefixes(inflectedPrefix, includeLiPrefix).flatMap((pre) => [
+        sandwichInflection(pre, deinflectedPrefix, inflectedSuffix, deinflectedSuffix, ['iv_p'], ['iv_d'], stemSegments),
+
+        // With attached direct object pronouns
+        ...(attachesTo1st ?
+            directObjectPronouns1st.map((p) => sandwichInflection(
+                pre,
+                deinflectedPrefix,
+                attachedSuffix + p,
+                deinflectedSuffix,
+                ['iv_p'],
+                ['iv_d'],
+                stemSegments,
+            )) :
+            []),
+        ...(attachesTo2nd ?
+            directObjectPronouns2nd.map((p) => sandwichInflection(
+                pre,
+                deinflectedPrefix,
+                attachedSuffix + p,
+                deinflectedSuffix,
+                ['iv_p'],
+                ['iv_d'],
+                stemSegments,
+            )) :
+            []),
+        ...directObjectPronouns3rd.map((p) => sandwichInflection(
+            pre,
+            deinflectedPrefix,
+            attachedSuffix + p,
+            deinflectedSuffix,
+            ['iv_p'],
+            ['iv_d'],
+            stemSegments,
+        )),
+    ]);
+
+    if (!deinflectedPrefix) {
+        const opts = {
+            attachedSuffix,
+            attachesTo1st,
+            attachesTo2nd,
+            includeLiPrefix,
+            initialStemSegment,
+            finalStemSegment,
+        };
+
+        // For Form IV, VII, VIII, IX, X, XI, XII, XIII, XIV, XV verbs
+        rules.push(
+            ...getImperfectRules(inflectedPrefix, 'أ', inflectedSuffix, deinflectedSuffix, opts),
+            ...getImperfectRules(inflectedPrefix, 'ا', inflectedSuffix, deinflectedSuffix, opts),
+        );
+    }
+
+    return rules;
 }
 
 /** @typedef {keyof typeof conditions} Condition */
@@ -84,7 +239,7 @@ const conditions = {
     v: {
         name: 'Verb',
         isDictionaryForm: true,
-        subConditions: ['pv'],
+        subConditions: ['pv', 'iv'],
     },
     pv: {
         name: 'Perfect Verb',
@@ -101,6 +256,23 @@ const conditions = {
     },
     pv_d: {
         name: 'Perfect Verb Dictionary Form (no affixes)',
+        isDictionaryForm: true,
+    },
+    iv: {
+        name: 'Imperfect Verb',
+        isDictionaryForm: true,
+        subConditions: ['iv_p', 'iv_s', 'iv_d'],
+    },
+    iv_p: {
+        name: 'Imperfect Verb with Prefix',
+        isDictionaryForm: false,
+    },
+    iv_s: {
+        name: 'Imperfect Verb with Suffix only',
+        isDictionaryForm: false,
+    },
+    iv_d: {
+        name: 'Imperfect Verb Dictionary Form (no affixes)',
         isDictionaryForm: true,
     },
 };
@@ -291,6 +463,116 @@ export const arabicTransforms = {
                 suffixInflection('وا', '', ['pv_s'], ['pv_d']),
                 ...directObjectPronouns.map((p) => suffixInflection(`و${p}`, '', ['pv_s'], ['pv_d'])),
             ],
+        },
+
+        // Imperfect Verb
+        'IVPref-hw': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 3rd. m. sing.',
+            rules: [...getImperfectRules('ي', '', '', '')],
+        },
+        'IVPref-hy': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 3rd. f. sing.',
+            rules: [...getImperfectRules('ت', '', '', '')],
+        },
+        'IVPref-hmA': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 3rd. m. dual',
+            rules: [
+                // Indicative
+                ...getImperfectRules('ي', '', 'ان', '', {includeLiPrefix: false}),
+                ...getImperfectRules('ي', '', 'آن', 'أ', {includeLiPrefix: false}),
+                // Subjunctive
+                ...getImperfectRules('ي', '', 'ا', ''),
+                ...getImperfectRules('ي', '', 'آ', 'أ'),
+            ],
+        },
+        'IVPref-hmA-ta': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 3rd. f. dual',
+            rules: [
+                // Indicative
+                ...getImperfectRules('ت', '', 'ان', '', {includeLiPrefix: false}),
+                ...getImperfectRules('ت', '', 'آن', 'أ', {includeLiPrefix: false}),
+                // Subjunctive
+                ...getImperfectRules('ت', '', 'ا', ''),
+                ...getImperfectRules('ت', '', 'آ', 'أ'),
+            ],
+        },
+        'IVPref-hm': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 3rd. m. pl.',
+            rules: [
+                // Indicative
+                ...getImperfectRules('ي', '', 'ون', '', {includeLiPrefix: false}),
+                // Subjunctive
+                ...getImperfectRules('ي', '', 'وا', '', {attachedSuffix: 'و'}),
+            ],
+        },
+        'IVPref-hn': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 3rd. f. pl.',
+            rules: [
+                ...getImperfectRules('ي', '', 'ن', '', {finalStemSegment: '(?<!ن)'}),
+                ...getImperfectRules('ي', '', 'ن', 'ن'),
+            ],
+        },
+        'IVPref-Anta': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 2nd. m. sing.',
+            rules: [...getImperfectRules('ت', '', '', '', {attachesTo2nd: false})],
+        },
+        'IVPref-Anti': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 2nd. f. sing.',
+            rules: [
+                ...getImperfectRules('ت', '', 'ين', '', {attachesTo2nd: false, includeLiPrefix: false}), // Indicative
+                ...getImperfectRules('ت', '', 'ي', '', {attachesTo2nd: false}), // Subjunctive
+            ],
+        },
+        'IVPref-AntmA': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 2nd. dual',
+            rules: [
+                // Indicative
+                ...getImperfectRules('ت', '', 'ان', '', {attachesTo2nd: false, includeLiPrefix: false}),
+                ...getImperfectRules('ت', '', 'آن', 'أ', {attachesTo2nd: false, includeLiPrefix: false}),
+                // Subjunctive
+                ...getImperfectRules('ت', '', 'ا', '', {attachesTo2nd: false}),
+                ...getImperfectRules('ت', '', 'آ', 'أ', {attachesTo2nd: false}),
+            ],
+        },
+        'IVPref-Antm': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 2nd. m. pl.',
+            rules: [
+                // Indicative
+                ...getImperfectRules('ت', '', 'ون', '', {attachesTo2nd: false, includeLiPrefix: false}),
+                // Subjunctive
+                ...getImperfectRules('ت', '', 'وا', '', {attachesTo2nd: false, attachedSuffix: 'و'}),
+            ],
+        },
+        'IVPref-Antn': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 2nd. f. pl.',
+            rules: [
+                ...getImperfectRules('ت', '', 'ن', '', {attachesTo2nd: false, finalStemSegment: '(?<!ن)'}),
+                ...getImperfectRules('ت', '', 'ن', 'ن', {attachesTo2nd: false}),
+            ],
+        },
+        'IVPref-AnA': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 1st. sing.',
+            rules: [
+                ...getImperfectRules('أ', '', '', '', {attachesTo1st: false}),
+                ...getImperfectRules('آ', 'أ', '', '', {attachesTo1st: false}),
+            ],
+        },
+        'IVPref-nHn': {
+            name: 'Imperfect Tense',
+            description: 'Imperfect Verb 1st. pl.',
+            rules: [...getImperfectRules('ن', '', '', '', {attachesTo1st: false})],
         },
     },
 };
