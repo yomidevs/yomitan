94503ab34233bcdddb075699a6a5f3f2870243b5
Jack Mitchell <jackmitc@usc.edu>
2025-02-21 14:48:32 -0600

    Remove console logs

--- ext/js/app/popup-proxy.js
+++ ext/js/app/popup-proxy.js
@@ -300,7 +300,7 @@ export class PopupProxy extends EventDispatcher {
      * @returns {Promise<boolean>} Whether the pointer is over the popup
      */
     isPointerOver() {
-        console.log('PROXY');
+
         return this._invokeSafe('popupFactoryIsPointerOver', {id: this._id}, false);
     }
 
