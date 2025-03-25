7a1fd1667fade41ba256eb5901ba692453267c8e
kuuuube <hexagonisalie@gmail.com>
2025-03-25 15:26:44 -0400

    Add more symbols

--- ext/js/language/ja/japanese-kana-romaji-dicts.js
+++ ext/js/language/ja/japanese-kana-romaji-dicts.js
@@ -325,7 +325,27 @@ export const ROMAJI_TO_HIRAGANA = {
     'u': 'う',
     'e': 'え',
     'o': 'お',
+
+    // Length 1 Special/Symbols
+    '.': '。',
+    ',': '、',
+    ':': '：',
+    '/': '・',
+    '!': '！',
+    '?': '？',
+    '~': '〜',
     '-': 'ー',
+    '‘': '「',
+    '’': '」',
+    '“': '『',
+    '”': '』',
+    '[': '［',
+    ']': '］',
+    '(': '（',
+    ')': '）',
+    '{': '｛',
+    '}': '｝',
+
 
     // n -> ん is a special case.
     'n': 'ん',
@@ -534,7 +554,26 @@ export const HIRAGANA_TO_ROMAJI = {
     ゑ: 'we',
     を: 'wo',
 
-    ー: '-',
+    // Length 1 Special/Symbols
+    '。': '.',
+    '、': ',',
+    '：': ':',
+    '・': '/',
+    '！': '!',
+    '？': '?',
+    '〜': '~',
+    'ー': '-',
+    '「': '‘',
+    '」': '’',
+    '『': '“',
+    '』': '”',
+    '［': '[',
+    '］': ']',
+    '（': '(',
+    '）': ')',
+    '｛': '{',
+    '｝': '}',
+    '　': ' ',
 
     // Length 1 Small - Even though these are usually represented with `x` or `l` prepending them, in romaji it makes the most sense to not do that
     ゃ: 'ya',
