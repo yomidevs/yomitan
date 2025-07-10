395b07058ce04d2b3a97f83e6fd20b8c8b9f51af
Kuuuube <61125188+Kuuuube@users.noreply.github.com>
2025-07-10 15:02:32 -0400

    Update settings page settings when profile is switched using action popup (#2084)
    * Update settings page settings when profile is switched using action popup
    
    * Fix typo in comments

--- ext/js/pages/settings/profile-controller.js
+++ ext/js/pages/settings/profile-controller.js
@@ -429,8 +429,9 @@ export class ProfileController {
 
         const settingsProfileIndex = this._settingsController.profileIndex;
 
-        // Udpate UI
+        // Update UI
         this._updateProfileSelectOptions();
+        void this.setDefaultProfile(profileCurrent);
 
         /** @type {HTMLSelectElement} */ (this._profileActiveSelect).value = `${profileCurrent}`;
 
@@ -441,7 +442,7 @@ export class ProfileController {
             void this._profileConditionsUI.prepare(settingsProfileIndex);
         }
 
-        // Udpate profile entries
+        // Update profile entries
         for (const entry of this._profileEntryList) {
             entry.cleanup();
         }
