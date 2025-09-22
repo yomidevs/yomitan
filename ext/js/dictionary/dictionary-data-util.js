c37180ca0496a4ad00fdfd9b4882b1581df91afd
Arbyste <arbyste@outlook.com>
2025-09-21 20:14:19 -0600

    Replacing tabs with spaces

--- ext/js/dictionary/dictionary-data-util.js
+++ ext/js/dictionary/dictionary-data-util.js
@@ -333,7 +333,7 @@ export function isNonNounVerbOrAdjective(wordClasses) {
             case 'vs':
                 isVerbOrAdjective = true;
                 isSuruVerb = true;
-		break;
+                break;
             case 'n':
                 isNoun = true;
                 break;
