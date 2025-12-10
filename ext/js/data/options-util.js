48ad06b3e6ed27ba4c19b9b17a9adb3cfb15955a
Kuuuube <61125188+Kuuuube@users.noreply.github.com>
2025-12-10 10:49:49 -0500

    Fix glossary-plain and glossary-plain-no-dictionary not working when Result grouping mode == No grouping (#2252)
    * Fix glossary-plain and glossary-plain-no-dictionary not working when resultOutputMode (Result grouping mode) == split (No grouping)
    
    * Write tests

--- ext/js/data/options-util.js
+++ ext/js/data/options-util.js
@@ -585,6 +585,7 @@ export class OptionsUtil {
             this._updateVersion71,
             this._updateVersion72,
             this._updateVersion73,
+            this._updateVersion74,
         ];
         /* eslint-enable @typescript-eslint/unbound-method */
         if (typeof targetVersion === 'number' && targetVersion < result.length) {
@@ -1820,6 +1821,14 @@ export class OptionsUtil {
         for (const profile of options.profiles) {
             profile.options.anki.targetTags = [];
         }
+    }
+
+    /**
+     *  - Fix glossary-plain and glossary-plain-no-dictionary not working when resultOutputMode (Result grouping mode) == split (No grouping)
+     *  @type {import('options-util').UpdateFunction}
+     */
+    async _updateVersion74(options) {
+        await this._applyAnkiFieldTemplatesPatch(options, '/data/templates/anki-field-templates-upgrade-v74.handlebars');
     }
 
     /**
