65dae606d92028a0760c4dcc4a6eba63fa9652de
Kuuuube <61125188+Kuuuube@users.noreply.github.com>
2025-12-29 11:54:44 -0500

    Don't allow `content` to get mutated when creating glossary-plain (#2274)
    * Disallow unlabeled string results
    
    * Don't shift in extractGlossaryStructuredContentRecursive to avoid mutation

--- ext/js/templates/anki-template-renderer.js
+++ ext/js/templates/anki-template-renderer.js
@@ -787,8 +787,8 @@ export class AnkiTemplateRenderer {
     _extractGlossaryStructuredContentRecursive(content) {
         /** @type {import('structured-content.js').Content[]} */
         const extractedContent = [];
-        while (content.length > 0) {
-            const structuredContent = content.shift();
+        for (let i = 0; i < content.length; i++) {
+            const structuredContent = content[i];
             if (Array.isArray(structuredContent)) {
                 extractedContent.push(...this._extractGlossaryStructuredContentRecursive(structuredContent));
             } else if (typeof structuredContent === 'object' && structuredContent) {
@@ -800,8 +800,6 @@ export class AnkiTemplateRenderer {
                 if (structuredContent.content) {
                     extractedContent.push(...this._extractGlossaryStructuredContentRecursive([structuredContent.content]));
                 }
-            } else if (typeof structuredContent === 'string') {
-                extractedContent.push(structuredContent);
             }
         }
 
