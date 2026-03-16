e00b4862563bc6457bcdc7129eca68b6f16d2646
Kuuuube <61125188+Kuuuube@users.noreply.github.com>
2026-03-16 17:08:49 -0400

    Allow using actual dictionary defined frequencyMode for auto detect frequency sorting mode (#2329)
    * Allow using actual dictionary defined frequencyMode for auto detect frequency sorting mode
    
    * Cleanup returns for _getFrequencyOrder, actually return the order

--- ext/js/pages/settings/sort-frequency-dictionary-controller.js
+++ ext/js/pages/settings/sort-frequency-dictionary-controller.js
@@ -142,18 +142,27 @@ export class SortFrequencyDictionaryController {
      */
     async _autoUpdateOrder(dictionary) {
         const order = await this._getFrequencyOrder(dictionary);
-        if (order === 0) { return; }
-        const value = (order > 0 ? 'descending' : 'ascending');
-        /** @type {HTMLSelectElement} */ (this._sortFrequencyDictionaryOrderSelect).value = value;
-        await this._setSortFrequencyDictionaryOrderValue(value);
+        if (order === null) { return; }
+        /** @type {HTMLSelectElement} */ (this._sortFrequencyDictionaryOrderSelect).value = order;
+        await this._setSortFrequencyDictionaryOrderValue(order);
     }
 
     /**
      * @param {string} dictionary
-     * @returns {Promise<number>}
+     * @returns {Promise<import('settings').SortFrequencyDictionaryOrder?>}
      */
     async _getFrequencyOrder(dictionary) {
         const dictionaryInfo = await this._settingsController.application.api.getDictionaryInfo();
+        const dictionaryFrequencyMode = dictionaryInfo.find(({title}) => title === dictionary)?.frequencyMode ?? '';
+        switch (dictionaryFrequencyMode) {
+            case 'occurrence-based': {
+                return 'descending';
+            }
+            case 'rank-based': {
+                return 'ascending';
+            }
+        }
+
         const dictionaryLang = dictionaryInfo.find(({title}) => title === dictionary)?.sourceLanguage ?? '';
 
         /** @type {Record<string, string[]>} */
@@ -219,7 +228,11 @@ export class SortFrequencyDictionaryController {
                 result += Math.sign(details1.maxValue - details2.minValue) + Math.sign(details1.minValue - details2.maxValue);
             }
         }
-        return Math.sign(result);
+
+        const resultSign = Math.sign(result);
+        if (resultSign > 0) { return 'descending'; }
+        if (resultSign < 0) { return 'ascending'; }
+        return null;
     }
 
     /**
