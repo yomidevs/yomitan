3440451aecb23a43f308857969c890a55ce34a91
Chrisyk <chrisko2003@gmail.com>
2026-05-19 01:31:45 -0300

    Add support for ankiCardFormats in Yomitan API (#2409)
    * Add support for ankiConfigs in Yomitan API
    
    - Introduced new ankiConfigsInput type to define profileIndex.
    - Implemented logic to retrieve Anki configuration and field templates based on the provided profile index, with error handling for invalid indices.
    
    * Refactor Anki configuration handling in Yomitan API
    
    - Update the result structure to exclude the API key while maintaining the configuration and field templates.
    
    * Updated ankiConfigs to ankiCardFormats in Yomitan API
    
    - Renamed the ankiConfigsInput type to ankiCardFormatsInput for clarity.
    - Specialized API call to only retrieve card formats based on profile index.
    
    * Resolved Linter complaints regarding blank lines
    
    * Fixed another Linter issue:
    - Added required NewLine character at the end of the file

--- types/ext/yomitan-api.d.ts
+++ types/ext/yomitan-api.d.ts
@@ -44,6 +44,10 @@ export type tokenizeInput = {
     scanLength: number;
 };
 
+export type ankiCardFormatsInput = {
+    profileIndex?: number;
+};
+
 export type remoteVersionResponse = {
     version: number;
 };
