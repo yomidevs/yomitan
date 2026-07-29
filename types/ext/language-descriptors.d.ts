4a8c653b5f2b37e4834226f2e92e2a750a819fd2
Charlotte <altronic25@protonmail.com>
2026-07-29 08:49:01 +0100

    Add other Celtic languages

--- types/ext/language-descriptors.d.ts
+++ types/ext/language-descriptors.d.ts
@@ -103,6 +103,9 @@ type AllTextProcessors = {
     bg: {
         pre: CapitalizationPreprocessors;
     };
+    br: {
+        pre: CapitalizationPreprocessors;
+    };
     cs: {
         pre: CapitalizationPreprocessors;
     };
@@ -158,6 +161,9 @@ type AllTextProcessors = {
             convertLatinToGreek: TextProcessor;
         };
     };
+    gv: {
+        pre: CapitalizationPreprocessors;
+    };
     haw: {
         pre: CapitalizationPreprocessors;
     };
@@ -207,6 +213,9 @@ type AllTextProcessors = {
     };
     km: Record<string, never>;
     kn: Record<string, never>;
+    kw: {
+        pre: CapitalizationPreprocessors;
+    };
     mn: {
         pre: CapitalizationPreprocessors;
     };
